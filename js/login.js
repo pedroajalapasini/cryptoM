01c17459743be6bb88074cf3b40e7b5da670ec80
Pedro Ajala Pasini <pedro.sebastian.pasini@gmail.com>
2022-11-21 21:55:01 -0300

    v2.6

--- js/login.js
+++ js/login.js
@@ -19,4 +19,29 @@ containerLogin.addEventListener('click', (event) => {
     event.stopPropagation()
 })
 
+/* formulario login */
+const formLogin = document.querySelector('#my-form1')
+const btnMail = formLogin.querySelector('#email-login')
+const btnPass = formLogin.querySelector('#pass-login')
+const btnIngresa = formLogin.querySelector('#btn-log')
 
+const usuario = []
+const ValidarCuenta = (mail) => {
+    return usuario.find(usuario => usuario.mail === mail) || {error: 'Debes registrarte'}
+}
+btnIngresa.addEventListener('click', () => {
+    ValidarCuenta()
+})
+
+/* formulario registrate */
+const formRegistre = document.querySelector('#my-form2')
+const inputMail = formRegistre.querySelector('#email-signup')
+const inputPass = formRegistre.querySelector('#pass-signup')
+const btnCrea = formRegistre.querySelector('#btn-signup')
+
+const CrearCuenta = ()=>{
+    usuario.push(inputMail,inputPass);
+}
+btnCrea.addEventListener('click', () => {
+    CrearCuenta()
+})
